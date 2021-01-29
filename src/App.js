4d6b869d48813190b0304ed01db9b3ce008b4f05
Sama Manalai <manalais@berea.edu>
2021-01-29 15:01:38 -0500

    added the remaining components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
-import React, {Components} from 'react';
+import React, {Component} from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+
+const app = new Clarifai.App({
+  apiKey: '25dabbe2a6864ff096f28c9634ec7f10'
+});
 
 const particlesOptions = {
   particles: {
@@ -20,7 +26,52 @@ const particlesOptions = {
   }              
 }
 
-function App() {
+class App extends Component{
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box:{},
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  } 
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+  }
+
+
+
+
+  render(){
       return (
         <div className="App">
           <Particles className='particles'
@@ -29,11 +80,14 @@ function App() {
           <Navigation/>
           <Logo/>
           <Rank/>
-          <ImageLinkForm/>
-          {/*<FaceRecognition/>*/}
+          <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit}
+          />
+          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
         </div>
-
   );
+}
 }
     
 
