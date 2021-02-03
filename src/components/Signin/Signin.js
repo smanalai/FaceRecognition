aa2254ed87be341652ca60d196df24b9ce4f8fd8
Sama Manalai <manalais@berea.edu>
2021-02-03 15:35:22 -0500

    initial commit

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Signin.css';
 
 
 class Signin extends React.Component{
@@ -38,47 +38,57 @@ class Signin extends React.Component{
 	}
 
 	render(){
-		  const {onRouteChange} = this.props;
-		  return(
-		  	<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw5 shadow-5 center">
-			  	<main className="pa4 black-80">
-				  <div className="measure">
-				    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-				      <legend className="f4 center fw6 ph0 mh0">Sign In</legend>
-				      <div className="mt3">
-				        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-				        <input 
-				        	className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-				        	type="email" 
-				        	name="email-address"  
-				        	id="email-address"
-				        	onChange={this.onEmailChange}/>
-				      </div>
-				      <div className="mv3">
-				        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-				        <input 
-				        	className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-				        	type="password" 
-				        	name="password"  
-				        	id="password"
-				        	onChange={this.onPasswrodChange}/>
-				      </div>
-				    </fieldset>
-				    <div className="">
-				      <input 
-				      	onClick={this.onSubmitSignIn}
-				      	className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-				      	type="submit" 
-				      	value="Sign in"/>
-				    </div>
-				    <div className="lh-copy mt3">
-				      <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
-				    </div>
-				  </div>
-				</main>
-			</article>
-			);
-	}
+	  const {onRouteChange} = this.props;
+	  return(
+	  	<div className="container">
+	  	  <div className="row justify-content-between">
+	  	    <div className="content-left">
+	  	      <h1> Face Search Engine</h1>
+	  	      <h2> Let's detect faces in your pictures</h2>
+	  	    </div>
+	  	    <div className="content-right">
+			  	<article className="br3 ba b--black-10 mv4 mw5 shadow-5 ">
+				  	<main className="pa4 black-80">
+					  <div className="measure">
+					    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+					      <legend className="f4 center fw6 ph0 mh0">Sign In</legend>
+					      <div className="mt3">
+					        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+					        <input 
+					        	className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+					        	type="email" 
+					        	name="email-address"  
+					        	id="email-address"
+					        	onChange={this.onEmailChange}/>
+					      </div>
+					      <div className="mv3">
+					        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+					        <input 
+					        	className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+					        	type="password" 
+					        	name="password"  
+					        	id="password"
+					        	onChange={this.onPasswrodChange}/>
+					      </div>
+					    </fieldset>
+					    <div className="">
+					      <input 
+					      	onClick={this.onSubmitSignIn}
+					      	className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+					      	type="submit" 
+					      	value="Sign in"/>
+					    </div>
+					    <div className="lh-copy mt3">
+					      <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+					    </div>
+					  </div>
+					</main>
+				</article>
+			</div>
+		  </div>
+		</div>
+		);
+}
 }
 
 export default Signin;
