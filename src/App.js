727e478cf5e55f611144bfdb93a08f797a3e42f9
Sama Manalai <manalais@berea.edu>
2021-02-04 02:11:14 -0500

    debugging

--- src/App.js
+++ src/App.js
@@ -25,30 +25,7 @@ const particlesOptions = {
   }              
 }
 
-// const initialState = {
-//       input: '',
-//       imageUrl: '',
-//       box:{},
-//       route: 'signin',
-//       isSignedIn: false,
-//       user: {
-//         id: '',
-//         name: '',
-//         email: '',
-//         entries: 0,
-//         joined: ''
-//       }
-// }
-
-
-class App extends Component{
-
-  constructor() {
-    super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box:[],
+const initialState = {
       route: 'signin',
       isSignedIn: false,
       user: {
@@ -58,6 +35,17 @@ class App extends Component{
         entries: 0,
         joined: ''
       }
+}
+
+
+class App extends Component{
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box:[]
 }
 }
   
@@ -112,7 +100,6 @@ class App extends Component{
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
-            input: this.state.input,
             id: this.state.user.id
           })
         })
@@ -130,7 +117,7 @@ class App extends Component{
 
   onRouteChange = (route) => {
     if(route === 'signout'){
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home'){
       this.setState({isSignedIn: true})
     }
